5c935216f0f2996d669850f62970b80faa1db3c6
HectorFraguasNateevo <hector.fraguas@nateevo.com>
2019-05-17 10:20:40 +0200

    prueba

--- routes/token.js
+++ routes/token.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jwt-simple');
 
-var payload = { foo: 'kasa' };
+
+router.get('/', function(req, res, next) {
+  
+var payload = { foo: 'adios' };
 var secret = 'XwdKtoBAV9fwBfFKCZCmTyCW';
+
+// HS256 secrets are typically 128-bit random strings, for example hex-encoded:
+// var secret = Buffer.from('fe1a1915a379f3be5394b64d14794932', 'hex')
 
 // encode
 var token = jwt.encode(payload, secret);
@@ -12,16 +18,7 @@ var token = jwt.encode(payload, secret);
 var decoded = jwt.decode(token, secret);
 console.log(decoded); //=> { foo: 'bar' }
 
-var encodedJWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmb28iOiJrYXNhIn0.O5sEbGHFbQ4ahl9jLfHkm-UkczGz55WsO9ZmG4h8NcA";
-var secret = "XwdKtoBAV9fwBfFKCZCmTyCW";
-var decodedJWT = jwt.decode(encodedJWT, secret);
-
-
-
-
-router.get('/', function(req, res, next) {
-  
-res.render('index', { title: JSON.stringify(token) +"|deco|"+ JSON.stringify(decodedJWT)+
+res.render('index', { title: JSON.stringify(token) +"||"+ JSON.stringify(decoded)+
       "||"+JSON.stringify(payload) });
 });
 
